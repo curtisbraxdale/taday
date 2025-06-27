2293803edb22a6caaf9a803ac33694f4ce6c1f79
curtisbraxdale <braxdalecurtis@gmail.com>
2025-06-27 12:01:49 -0400

    Created agenda logic, twilio integration wip.

--- cmd/sender/main.go
+++ cmd/sender/main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"context"
+	"database/sql"
+	"log"
+	"os"
+	"time"
 
+	"github.com/curtisbraxdale/taday/internal/database"
+	"github.com/curtisbraxdale/taday/internal/handlers"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DATABASE_URL")
+	platform := os.Getenv("PLATFORM")
+	secret := os.Getenv("SECRET")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("Error connecting to database: %v", err)
+	}
+	dbQueries := database.New(db)
+	apiCfg := handlers.ApiConfig{Queries: dbQueries, Platform: platform, Secret: secret}
+
+	day := time.Now().Weekday().String()
+	userIDs, err := apiCfg.Queries.GetAllUserIDs(context.Background())
+	if err != nil {
+		log.Fatalf("Error getting userIDs: %s", err)
+	}
+	if day == "Monday" {
+		for _, id := range userIDs {
+			agenda, err := apiCfg.CreateWeeklyAgenda(id)
+			if err != nil {
+				log.Printf("Error creating agenda for user %v : %s", id, err)
+			}
+			// TWILIO API SEND TEXT
+		}
+	} else {
+		for _, id := range userIDs {
+			agenda, err := apiCfg.CreateDailyAgenda(id)
+			if err != nil {
+				log.Printf("Error creating agenda for user %v : %s", id, err)
+			}
+			// TWILIO API SEND TEXT
+		}
+	}
 }
---
+++ internal/handlers/agenda.go
@@ -0,0 +1,75 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/curtisbraxdale/taday/internal/database"
+	"github.com/google/uuid"
+)
+
+func (cfg *ApiConfig) CreateDailyAgenda(userID uuid.UUID) (string, error) {
+	now := time.Now()
+	dbToDos, err := cfg.Queries.GetTodosByUserID(context.Background(), userID)
+	if err != nil {
+		log.Printf("Error getting ToDos for user %v: %s", userID, err)
+		return "", err
+	}
+	dbEventParams := database.GetUserEventsTodayParams{UserID: userID, Date: now, DatePlus1Day: now.Add(24 * time.Hour)}
+	dbEvents, err := cfg.Queries.GetUserEventsToday(context.Background(), dbEventParams)
+	if err != nil {
+		log.Printf("Error getting Events for user %v: %s", userID, err)
+		return "", err
+	}
+	agendaString := "=== TADAYs AGENDA ===\n\n"
+	for _, t := range dbToDos {
+		if t.Description.Valid {
+			agendaString += fmt.Sprintf("%v\n+%v\n\n", t.Title, t.Description.String)
+		} else {
+			agendaString += fmt.Sprintf("%v\n\n", t.Title)
+		}
+	}
+	for _, e := range dbEvents {
+		if e.Description.Valid {
+			agendaString += fmt.Sprintf("%v\n+%v\n\n", e.Title, e.Description.String)
+		} else {
+			agendaString += fmt.Sprintf("%v\n\n", e.Title)
+		}
+	}
+	agendaString += fmt.Sprint("=====================\n")
+	return agendaString, nil
+}
+
+func (cfg *ApiConfig) CreateWeeklyAgenda(userID uuid.UUID) (string, error) {
+	now := time.Now()
+	dbToDos, err := cfg.Queries.GetTodosByUserID(context.Background(), userID)
+	if err != nil {
+		log.Fatalf("Error getting ToDos for user %v: %s", userID, err)
+		return "", err
+	}
+	dbEventParams := database.GetUserEventsWeekParams{UserID: userID, Date: now, DatePlus7Days: now.Add(7 * 24 * time.Hour)}
+	dbEvents, err := cfg.Queries.GetUserEventsWeek(context.Background(), dbEventParams)
+	if err != nil {
+		log.Fatalf("Error getting Events for user %v: %s", userID, err)
+		return "", err
+	}
+	agendaString := "=== TADAYs AGENDA ===\n\n"
+	for _, t := range dbToDos {
+		if t.Description.Valid {
+			agendaString += fmt.Sprintf("%v\n+%v\n\n", t.Title, t.Description.String)
+		} else {
+			agendaString += fmt.Sprintf("%v\n\n", t.Title)
+		}
+	}
+	for _, e := range dbEvents {
+		if e.Description.Valid {
+			agendaString += fmt.Sprintf("%v\n+%v\n\n", e.Title, e.Description.String)
+		} else {
+			agendaString += fmt.Sprintf("%v\n\n", e.Title)
+		}
+	}
+	agendaString += fmt.Sprint("=====================\n")
+	return agendaString, nil
+}
